e247affc39ec9d9658bab22fc2cae7661abb6de2
Panos Nikolakakis <nikolakakispanos@gmail.com>
2024-02-05 20:53:22 +0200

    Initial commit

---
+++ src/main/java/com/panosnikolakakis/coordmanager/CoordManager.java
@@ -0,0 +1,16 @@
+package com.panosnikolakakis.coordmanager;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CoordManager implements ModInitializer {
+	public static final String MOD_ID = "coordmanager";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
